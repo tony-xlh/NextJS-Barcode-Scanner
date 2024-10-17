d81c2201771141decee98b305c27e1c96d4d6549
xulihang <xulihanghai@163.com>
2024-10-17 15:42:39 +0800

    fix DBR and DCE's versions

--- src/components/BarcodeScanner.tsx
+++ src/components/BarcodeScanner.tsx
@@ -30,7 +30,7 @@ const BarcodeScanner = (props:ScannerProps): React.ReactElement => {
         }else{
           BarcodeReader.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
         }
-        BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@9.6.42/dist/";
+        BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@9.6.11/dist/";
       }
       reader.current = await BarcodeReader.createInstance();
       enhancer.current = await CameraEnhancer.createInstance();
